2dc33a5f8007d32088a8d219f54781aecc835789
Shajid-Mahmud-CS <shajidmahmud.ruet.cs@gmail.com>
2026-03-10 09:54:13 +0600

    second

--- src/screen/HomePage.tsx
+++ src/screen/HomePage.tsx
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import eid from './../assets/eid.jpg'
+import em from './../assets/em.jpg'
 import x from './../assets/x.jpg'
+import bkash from './../assets/bkash.gif'
 import ReactConfetti from 'react-confetti';
+import mp from '../assets/mp.mp3'
 
 export default function HomePage() {
     const number = "01902517378"
+    const audioRef = useRef<HTMLAudioElement | null>(null);
     const [state, setState] = useState(false);
     const [press, setPress] = useState(false);
     const [windowSize, setWindowSize] = useState({
         width: typeof window !== 'undefined' ? window.innerWidth : 0,
         height: typeof window !== 'undefined' ? window.innerHeight : 0
     })
+    const handleButtonClick = () => {
+        setPress(true);
+        if (audioRef.current) {
+            audioRef.current.currentTime = 127; // start at 60 seconds
+            audioRef.current.play();
+        }
+    }
 
     useEffect(() => {
         // Update window size on resize
@@ -53,45 +64,31 @@ export default function HomePage() {
                     colors={['#FFB800', '#FF0000', '#00FFAA', '#00A0FF']}
                 />
             )}
-            <div className='h-full w-full flex justify-center items-center flex-col gap-12 md:gap-20'>
-                <div className='relative bg-amber-100 gap-10 md:gap-[20px] h-[50%] sm:h-[55%] md:h-[40%] w-[90%] sm:w-[70%] md:w-[40%] flex flex-col items-center justify-center rounded-3xl shadow-lg shadow-gray-200 p-4 sm:p-6'>
-                    <div className='text-center'>
-                        <h1 className='text-[20px] sm:text-[22px] md:text-[22px] font-[poppins]'>" ঈদের চাঁদ আকাশে</h1>
-                        <h1 className='text-[20px] sm:text-[22px] md:text-[22px] font-[poppins]'>সালামি দিন বিকাশে 🌙 "</h1>
+            <audio ref={audioRef} src={mp} loop />
+            <div className='h-screen w-screen bg-transparent flex flex-col justify-center items-center'>
+                <div className='relative flex flex-col w-[80%] h-[30%] bg-amber-100 rounded-2xl justify-center items-center md:w-[400px] md:h-[300px] md:bg-amber-100 md:flex md:flex-col md:justify-center md:items-center'>
+                    <div className='w-[70%] flex  flex-col justify-center items-center'>
+                        <h1 className='text-[19px] font-[poppins] font-bold'>" ঈদের চাঁদ আকাশে,</h1>
+                        <h1 className='text-[19px] font-[poppins] font-bold'>সালামি দিন বিকাশে "</h1>
                     </div>
-                    <div className='flex flex-row gap-4 sm:gap-6 md:gap-[10px] items-center mt-2 sm:mt-4'>
-                        <h1 className='text-[18px] sm:text-[20px] md:text-[25px] font-[poppins] font-medium'>{number}</h1>
-                        <button
-                            onClick={copyNumber}
-                            className="text-sm sm:text-base md:text-xl hover:scale-110 transition h-8 sm:h-10 md:h-[30px] w-20 sm:w-24 md:w-[90px] bg-black rounded-2xl flex items-center justify-center"
-                        >
-                            <h1 className='text-white font-[poppins] font-medium'>{state ? 'Copied' : 'Copy'}</h1>
+                    <div className='flex flex-row items-center gap-[5px] md:gap-[10px] w-[70%] justify-center'>
+                        <img src={bkash} className='h-[50px] w-[50px] md:h-[80px] md:w-[80px]' />
+                        <h1 className='text-[19px] font-[poppins] font-bold'>{number}</h1>
+                        <button className='ml-[20px] bg-black py-[5px] px-[10px] rounded-lg' onClick={copyNumber}>
+                            <h1 className='text-[13px] md:text-[15px] font-[poppins] font-medium text-white'>{state ? 'Copied' : 'Copy'}</h1>
                         </button>
                     </div>
-
-                    <div className='h-full w-full absolute bg-black top-0 left-0 z-30 rounded-3xl flex flex-col items-center justify-center'
-                        style={{
-                            zIndex: press ? -30 : 30,
-                        }}
-                    ></div>
-
-                    <div className='h-full w-full absolute top-0 left-0 z-40 rounded-3xl flex flex-col items-center justify-center animate-pulse'
-                        style={{
-                            backgroundImage: `url(${eid})`,
-                            backgroundSize: "100% 120%",
-                            zIndex: press ? -30 : 40,
-                        }}>
+                    <div className='absolute bg-white top-0 left-0 w-[400px] h-[300px] flex items-center justify-center z-20 rounded-2xl' style={{ zIndex: press ? -20 : 20 }}>
+                    </div>
+                    <div className='absolute top-0 left-0 w-[400px] h-[300px] flex items-center justify-center z-30 animate-pulse rounded-2xl' style={{ zIndex: press ? -30 : 30 }}>
+                        <img src={em} className='w-full h-full block rounded-2xl' />
                     </div>
                 </div>
-
-                {
-                    !press && (
-                        <button className='bg-gray-700 h-12 sm:h-14 md:h-[50px] w-72 sm:w-80 md:w-[300px] rounded-3xl animate-bounce flex items-center justify-center' onClick={() => PressBtn()}>
-                            <h1 className='font-[poppins] text-[16px] sm:text-[18px] md:text-[20px] font-medium text-white'>কার্ডটি খুলুন 🌙</h1>
-                        </button>
-                    )
-                }
+                <button className='bg-cyan-900 py-[10px] px-[20px] rounded-full mt-[30px] animate-bounce' onClick={handleButtonClick}>
+                    <h1 className='text-[16px] text-white font-[poppins] font-bold'>ভিতরে কী আছে জানতে বক্সটি খুলুন 👀🎁</h1>
+                </button>
             </div>
+
         </div>
     )
 }

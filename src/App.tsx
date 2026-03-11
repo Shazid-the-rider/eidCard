fa4d0587398124f4c79a8a033039d105fc85df8a
Shajid-Mahmud-CS <shajidmahmud.ruet.cs@gmail.com>
2026-03-11 10:21:55 +0600

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react'
 import './App.css'
 import HomePage from './screen/HomePage'
 
 function App() {
+  // globally calculate --vh to avoid broken layouts in mobile in‑app browsers
+  useEffect(() => {
+    const setRealHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setRealHeight();
+    window.addEventListener('resize', setRealHeight);
+    return () => window.removeEventListener('resize', setRealHeight);
+  }, []);
 
   return (
     <>
-      <HomePage/>
+      <HomePage />
     </>
-  )
+  );
 }
 
 export default App
